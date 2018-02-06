f4a890b5c6702fdb312df3a6d28258d1f2afbfd1
Emre <yunusemreorcun@protonmail.com>
2018-02-07 00:29:30 +0300

    Print etmede iyileştirme

--- mubiscli.py
+++ mubiscli.py
@@ -9,8 +9,10 @@ import requests
 import codecs
 import dateutil.parser as dparse
 import json
+import calendar
 from bs4 import BeautifulSoup
 from datetime import datetime
+from datetime import time
 from mubis import tarih_saturday as satu
 from mubis import tarih_monday as mond
 
@@ -65,12 +67,12 @@ def listing(jfile):
 def outputing(gun_programi):
 
     for entry in gun_programi:
-        ders = entry['title']
-        start = dparse.parse(entry['start'].split("+")[0])
-        end =dparse.parse(entry['end'].split("+")[0])
-        print("{0} - {1}\n\t{2}\n".format(start, end, ders))
-
-
+        ders = entry['title']                                                           # Parse, JSON'daki tarihi düzgün göstermeye yarıyor.
+        _date=dparse.parse(entry['start'].split("T")[0]).date()                         # Split(T)[0], Zaman bilgisindeki tarih kısmını alıyor
+        _end_h=dparse.parse(entry['end'].split("+")[0]).time().strftime("%H:%M")        # date() eklendi çünkü parse datetime objesi oluşturuyor ve saatleri 0 yapıyor.
+        _start_h = dparse.parse(entry['start'].split("+")[0]).time().strftime("%H:%M")   # Split(+)[0], Zaman ve tarih bilgisini direk alıyor.
+        _date_int = calendar.day_name[_date.weekday()]
+        print("{0}-{1}\n{2}-{3}:{4}".format(_date,_date_int,_start_h,_end_h,ders))
 with requests.Session() as s:
     payload = usr_inf()
     p = s.post(user_sessions, data=payload)
@@ -93,3 +95,6 @@ with requests.Session() as s:
             print("Bir problem var. Dosyaya yazılamadı.")
         jfile = codecs.open("takvim.json", mode="r", encoding="utf-8")
         outputing(listing(jfile))
+
+#jfile = codecs.open("takvim.json", mode="r", encoding="utf-8")
+#outputing(listing(jfile))
